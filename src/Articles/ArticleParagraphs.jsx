d21d9ee05adbdbdb471ce1cee68453fd1ac200b6
Craig McLeod <mcleod.craig@outlook.com>
2024-01-28 15:09:23 +0000

    creating article paragraphs component

---
+++ src/Articles/ArticleParagraphs.jsx
@@ -0,0 +1,14 @@
+const parser = new DOMParser()
+
+export const ArticleParagraphs = ({ article }) => {
+    return (
+        // using dom parser to pull p tags textContent from contentful
+        [...parser.parseFromString(article?.fields?.fullArticle, 'text/html').body.children].map(x => {
+            return (
+                <p>
+                    {x.textContent}
+                </p>
+            )
+        })
+    )
+}
