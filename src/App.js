927a036f34b94cd4bb1ef15cd153bfcf340f95fc
Craig McLeod <mcleod.craig@outlook.com>
2024-01-28 15:10:17 +0000

    Implementing articleContainer in app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 import axios from 'axios'
 import { LandingContainer } from './Landing/LandingContainer';
 import FoldArticleButton from './Articles/FoldArticleButton';
-import { ExternalLinksContainer } from './Articles/ExternalLinksContainer';
-import { ReadOnButton } from './Articles/ReadOnButton';
-const parser = new DOMParser()
+import { ArticleContainer } from './Articles/ArticleContainer';
 
 
 function App() {
@@ -27,40 +25,15 @@ function App() {
           <FoldArticleButton articleOpenIndex={articleOpenIndex} setArticleOpenIndex={setArticleOpenIndex} />
           {articles && articles?.items?.sort((a, b) => new Date(b.fields.publishedDate).getTime() - new Date(a.fields.publishedDate).getTime()).map((article, i) => {
             return (
-
-              <article id='regular-article' className={`article ${articleOpenIndex === i ? 'open' : 'closed'} ${article?.metadata.tags.some(z => z.sys.id === 'featured') ? '' : 'other-articles'}`}>
-                <div className='article-image-wrapper'>
-                  <img alt="" className='article-image' src={`https:${articles.includes.Asset.find(y => y.sys.id === article.fields.image.sys.id).fields.file.url}`} />
-                </div>
-                <div className='text-blocks'>
-                  <h2 className='alt-font'>
-                    {article?.fields?.articleHeader}
-                  </h2>
-                  <p className='published-date'>
-                    <b>Published</b> {new Date(article?.fields?.publishedDate).toLocaleDateString()}
-                  </p>
-                  {articleOpenIndex === i ?
-                    <>
-                      {[...parser.parseFromString(article?.fields?.fullArticle, 'text/html').body.children].map(x => {
-                        return <p>
-                          {x.textContent}
-                        </p>
-                      })}
-                      {<ExternalLinksContainer article={article} />}
-                    </>
-                    :
-                    <>
-                      <p>
-                        <i> {article?.fields?.blurb} </i>
-                      </p>
-                      <ReadOnButton setArticleOpenIndex={setArticleOpenIndex} i={i} />
-                    </>
-                  }
-                </div>
-              </article>
+              <ArticleContainer
+                articleOpenIndex={articleOpenIndex}
+                setArticleOpenIndex={setArticleOpenIndex}
+                i={i}
+                article={article}
+                articles={articles}
+              />
             )
           })}
-
         </section>
       </main >
     </div >
