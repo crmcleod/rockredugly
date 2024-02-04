f6da830ec3ecbb82358588931fa5592b6856887b
Craig McLeod <mcleod.craig@outlook.com>
2024-02-04 22:32:55 +0000

    Fixed scroll issue and reinstated timeout

--- src/Articles/ReadOnButton.jsx
+++ src/Articles/ReadOnButton.jsx
@@ -1,14 +1,17 @@
-export const ReadOnButton = ({setArticleOpenID, id}) => {
-    return(
-        <button className='alt-font letter-space-1' onClick={(e) => {
-            setArticleOpenID(id)
-            const parent = e.target.parentNode.parentNode
-            if (parent) {
-              console.log('scroll parent block')
-              document.querySelector('.regular-article' + id)
-              .scrollIntoView({ behavior: 'instant', block: 'start' }, 100)            }
-          }}>
-            Read on
-          </button>
-    )
+export const ReadOnButton = ({ setArticleOpenID, id }) => {
+  return (
+    <button className='alt-font letter-space-1' onClick={(e) => {
+      setArticleOpenID(id)
+      const parent = e.target.parentNode.parentNode
+      if (parent) {
+        setTimeout(() => {
+          document.querySelector('.regular-article' + id)
+            .scrollIntoView({ behavior: 'instant', block: 'start' })
+        }, 50)
+      }
+    }
+    }>
+      Read on
+    </button>
+  )
 }
