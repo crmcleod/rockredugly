37dfb17be8b32f35ab8eeed85b1c10caf58f1c53
Craig McLeod <mcleod.craig@outlook.com>
2024-04-14 13:51:09 +0100

    Remove unecessary key element; prevent bot from local

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   }, [notification])
 
   useEffect(() => {
-    axios.post(`https://discord.com/api/webhooks/${process.env.REACT_APP_channel_id}/${process.env.REACT_APP_channel_key}`, { "content": `${new Date()}` })
+   (!process.env.REACT_APP_local && axios.post(`https://discord.com/api/webhooks/${process.env.REACT_APP_channel_id}/${process.env.REACT_APP_channel_key}`, { "content": `${new Date()}` }))
     axios.get(`https://cdn.contentful.com/spaces/${process.env.REACT_APP_space_id}/environments/master/entries?access_token=${process.env.REACT_APP_access_token}&metadata.tags.sys.id[all]=${process.env.REACT_APP_content_tag}`)
       .then(x => {
         const incomingArticles = x.data
@@ -107,7 +107,7 @@ function App() {
             return (
               <ArticleContainer
                 setNotification={setNotification}
-                key={article.sys.id + articleOpenID}
+                key={article.sys.id}
                 articleOpenID={articleOpenID}
                 setArticleOpenID={setArticleOpenID}
                 i={i}
