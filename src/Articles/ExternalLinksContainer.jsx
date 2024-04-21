566beb766e2a237f4bf5d8280896b8b49206a7b4
Craig McLeod <mcleod.craig@outlook.com>
2024-04-21 16:41:29 +0100

    Handling alternative link structure

--- src/Articles/ExternalLinksContainer.jsx
+++ src/Articles/ExternalLinksContainer.jsx
@@ -1,8 +1,11 @@
 export const ExternalLinksContainer = ({ article }) => {
+
+    const articleFields = article?.fields
+
     return (
         <div className='external-links-wrapper'>
-            {article?.fields?.links?.map((link, i) => {
-                return <a key={i+new Date().getTime()} className='external-links' href={link}>{link}</a>
+            {(articleFields?.linksV2 ?? articleFields?.links)?.map((link, i) => {
+                return <a key={i + new Date().getTime()} className='external-links' href={link?.url ?? link}>{link?.summary ?? link}</a>
             })}
         </div>
     )
